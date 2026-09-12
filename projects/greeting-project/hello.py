2f543f2c28c1007efcb9e4232b76df7be0bb0c72
nwu13-code <nwu13@horizon.csueastbay.edu>
2026-09-12 14:55:04 -0700

    Update hello.py

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,7 +1,18 @@
 
+from datetime import datetime
+
 def greet_user():
     name = input("Enter your name: ")
-    print(f"Hello, {name}! Welcome to Data Science.")
+
+    hour = datetime.now().hour
+    if hour < 12:
+        time_hello = "Good morning"
+    elif hour < 18:
+        time_hello = "Good afternoon"
+    else:
+        time_hello = "Good evening"
+
+    print(f"{time_hello}, {name}! Welcome to Data Science.")
 
 if __name__ == "__main__":
     greet_user()
