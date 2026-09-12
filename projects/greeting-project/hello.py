68f9d15eaf6e1f02a13dae9227f94b8edb3db7a9
nwu13-code <nwu13@horizon.csueastbay.edu>
2026-09-12 15:03:28 -0700

    Update hello.py
    add eaten code

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,8 +1,10 @@
 
+# My first Python project in GitHub
 from datetime import datetime
 
 def greet_user():
     name = input("Enter your name: ")
+    eaten = input("Eat before？(yes/no): ")
 
     hour = datetime.now().hour
     if hour < 12:
@@ -13,6 +15,11 @@ def greet_user():
         time_hello = "Good evening"
 
     print(f"{time_hello}, {name}! Welcome to Data Science.")
+
+    if eaten.lower() in ["yes", "y"]:
+        print(f"OKAY,{name}")
+    else:
+        print(f"{name}，let me cook for you")
 
 if __name__ == "__main__":
     greet_user()
